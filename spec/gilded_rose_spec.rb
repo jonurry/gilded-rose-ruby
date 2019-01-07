23d40dbf7966112bf953436f06901c2aa3050454
Jon Urry <jon@urry.me>
2019-01-07 17:06:42 +0000

    Tests for basic item

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -7,5 +7,17 @@ describe GildedRose do
       GildedRose.new(items).update_quality
       expect(items[0].name).to eq 'foo'
     end
+
+    it 'quality reduces by 1' do
+      items = [Item.new('foo', 2, 2)]
+      GildedRose.new(items).update_quality
+      expect(items[0].quality).to eq 1
+    end
+
+    it 'sell_in reduces by 1' do
+      items = [Item.new('foo', 2, 2)]
+      GildedRose.new(items).update_quality
+      expect(items[0].sell_in).to eq 1
+    end
   end
 end
