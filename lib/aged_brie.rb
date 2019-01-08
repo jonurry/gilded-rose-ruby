b1243bfa6b4175ba15904c25b8953cd5cbb7ab87
Jon Urry <jon@urry.me>
2019-01-08 12:58:02 +0000

    Remove parts of AgedBrie class that are not overrides

--- lib/aged_brie.rb
+++ lib/aged_brie.rb
@@ -1,24 +1,10 @@
 require_relative 'generic_item'
 
-# GenericItem
+# AgedBrie
 class AgedBrie < GenericItem
   DAILY_QUALITY_INCREMENT = 1
-  DAILY_SELL_IN_INCREMENT = -1
 
   def initialize(name, sell_in, quality)
     super(name, sell_in, quality)
-  end
-
-  def update
-    update_quality
-    update_sell_in
-  end
-
-  def update_quality
-    @quality += DAILY_QUALITY_INCREMENT
-  end
-
-  def update_sell_in
-    @sell_in += DAILY_SELL_IN_INCREMENT
   end
 end
