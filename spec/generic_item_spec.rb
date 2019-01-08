46a6dd9dcbed2a54807fd93419fbdbfd33dd305e
Jon Urry <jon@urry.me>
2019-01-08 17:07:45 +0000

    Extract GenericItem tests to separate file

---
+++ spec/generic_item_spec.rb
@@ -0,0 +1,39 @@
+require_relative '../lib/generic_item'
+
+describe GenericItem do
+  describe '#update' do
+    it 'does not change the name' do
+      items = [GenericItem.new('foo', 0, 0)]
+      GildedRose.new(items).update_quality
+      expect(items[0].name).to eq 'foo'
+    end
+
+    it 'quality cannot be negative' do
+      items = [GenericItem.new('foo', 2, 0)]
+      GildedRose.new(items).update_quality
+      expect(items[0].quality).to eq 0
+    end
+
+    it 'sell_in reduces by 1' do
+      items = [GenericItem.new('foo', 2, 2)]
+      GildedRose.new(items).update_quality
+      expect(items[0].sell_in).to eq 1
+    end
+
+    context 'in date' do
+      it 'quality reduces by 1' do
+        items = [GenericItem.new('foo', 2, 2)]
+        GildedRose.new(items).update_quality
+        expect(items[0].quality).to eq 1
+      end
+    end
+
+    context 'past sell_in date' do
+      it 'quality reduces by 2' do
+        items = [GenericItem.new('foo', 0, 5)]
+        GildedRose.new(items).update_quality
+        expect(items[0].quality).to eq 3
+      end
+    end
+  end
+end
--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -2,42 +2,6 @@ require_relative '../lib/gilded_rose'
 
 describe GildedRose do
   describe '#update_quality' do
-    context 'Generic Item' do
-      it 'does not change the name' do
-        items = [GenericItem.new('foo', 0, 0)]
-        GildedRose.new(items).update_quality
-        expect(items[0].name).to eq 'foo'
-      end
-
-      it 'quality cannot be negative' do
-        items = [GenericItem.new('foo', 2, 0)]
-        GildedRose.new(items).update_quality
-        expect(items[0].quality).to eq 0
-      end
-
-      it 'sell_in reduces by 1' do
-        items = [GenericItem.new('foo', 2, 2)]
-        GildedRose.new(items).update_quality
-        expect(items[0].sell_in).to eq 1
-      end
-
-      context 'in date' do
-        it 'quality reduces by 1' do
-          items = [GenericItem.new('foo', 2, 2)]
-          GildedRose.new(items).update_quality
-          expect(items[0].quality).to eq 1
-        end
-      end
-
-      context 'past sell_in date' do
-        it 'quality reduces by 2' do
-          items = [GenericItem.new('foo', 0, 5)]
-          GildedRose.new(items).update_quality
-          expect(items[0].quality).to eq 3
-        end
-      end
-    end
-
     context 'Aged Brie' do
       it 'quality increases as it gets older' do
         items = [AgedBrie.new('Aged Brie', 5, 5)]
