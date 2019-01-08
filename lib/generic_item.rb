4ee4e9c96bd451f459a3265b7251be570502cc3b
Jon Urry <jon@urry.me>
2019-01-08 12:24:41 +0000

    Replace magic numbers with constants

--- lib/generic_item.rb
+++ lib/generic_item.rb
@@ -2,6 +2,9 @@ require_relative 'item'
 
 # GenericItem
 class GenericItem < Item
+  DAILY_QUALITY_INCREMENT = -1
+  DAILY_SELL_IN_INCREMENT = -1
+
   def initialize(name, sell_in, quality)
     super(name, sell_in, quality)
   end
@@ -12,10 +15,10 @@ class GenericItem < Item
   end
 
   def update_quality
-    @quality -= 1
+    @quality += DAILY_QUALITY_INCREMENT
   end
 
   def update_sell_in
-    @sell_in -= 1
+    @sell_in += DAILY_SELL_IN_INCREMENT
   end
 end
