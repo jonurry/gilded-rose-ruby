43562f4d7cb898aed598f433d78b1504265760d5
Jon Urry <jon@urry.me>
2019-01-08 19:39:37 +0000

    Tidy up Sulfuras to have empty methods and no instance variables

--- lib/sulfuras.rb
+++ lib/sulfuras.rb
@@ -4,7 +4,9 @@ require_relative 'generic_item'
 class Sulfuras < GenericItem
   def initialize(name, sell_in, quality)
     super(name, sell_in, quality)
-    @daily_quality_increment = 0
-    @daily_sell_in_increment = 0
   end
+
+  def update_quality; end
+
+  def update_sell_in; end
 end
