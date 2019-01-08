785bec6d5add6d9603407420a16399e05d840cde
Jon Urry <jon@urry.me>
2019-01-08 12:13:07 +0000

    Delegate item quality calculation for generic items

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -12,7 +12,7 @@ class GildedRose
       if item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert'
         if item.quality > 0
           if item.name != 'Sulfuras, Hand of Ragnaros'
-            item.quality = item.quality - 1
+            item.update_quality
           end
         end
       else
--- texttest_fixture.rb
+++ texttest_fixture.rb
@@ -2,16 +2,16 @@ require_relative './lib/gilded_rose'
 
 items = [
   # name, sell_in, quality
-  Item.new('+5 Dexterity Vest', 10, 20),
+  GenericItem.new('+5 Dexterity Vest', 10, 20),
   Item.new('Aged Brie', 2, 0),
-  Item.new('Elixir of the Mongoose', 5, 7),
+  GenericItem.new('Elixir of the Mongoose', 5, 7),
   Item.new('Sulfuras, Hand of Ragnaros', 0, 80),
   Item.new('Sulfuras, Hand of Ragnaros', -1, 80),
   Item.new('Backstage passes to a TAFKAL80ETC concert', 15, 20),
   Item.new('Backstage passes to a TAFKAL80ETC concert', 10, 49),
   Item.new('Backstage passes to a TAFKAL80ETC concert', 5, 49),
   # This Conjured item does not work properly yet
-  Item.new('Conjured Mana Cake', 3, 6), # <-- :O
+  GenericItem.new('Conjured Mana Cake', 3, 6), # <-- :O
 ]
 
 days = 2
