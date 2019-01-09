6261ec9e927e078edbb6226aafd289559384db04
Jon Urry <jon@urry.me>
2019-01-09 14:20:27 +0000

    Switch all tests to individual classes
    Doing this tests the individual class logic rather than the Gilded Rose update_quality method
    Add remaining logic to BackstagePass
    Override update_quality so that quality increment varies with sell_in date
    Private method to calculate quality increment.

--- lib/backstage_pass.rb
+++ lib/backstage_pass.rb
@@ -4,6 +4,22 @@ require_relative 'generic_item'
 class BackstagePass < GenericItem
   def initialize(name, sell_in, quality)
     super(name, sell_in, quality)
-    @daily_quality_increment = 1
+  end
+
+  def update_quality
+    @daily_quality_increment = calculate_quality_increment
+    @quality += @daily_quality_increment
+    @quality = 0 if @quality < 0
+    @quality = 50 if @quality > 50
+  end
+
+  private
+
+  def calculate_quality_increment
+    return 1 if @sell_in > 10
+    return 2 if @sell_in > 5
+    return -@quality if @sell_in <= 0
+
+    3
   end
 end
--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -11,46 +11,6 @@ class GildedRose
   end
 
   def update_quality
-    @items.each do |item|
-      if item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert'
-        item.update_quality
-      else
-        if item.quality < 50
-          item.update_quality
-          if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != 'Sulfuras, Hand of Ragnaros'
-        item.update_sell_in
-      end
-      if item.sell_in < 0
-        if item.name != 'Aged Brie'
-          if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-            if item.quality > 0
-              if item.name != 'Sulfuras, Hand of Ragnaros'
-                item.update_quality
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.update_quality
-          end
-        end
-      end
-    end
+    @items.each(&:update)
   end
 end
