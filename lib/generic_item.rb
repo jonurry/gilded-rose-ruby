7140599c7748a056e1040e3022c46d7a83c0e414
Jon Urry <jon@urry.me>
2019-01-09 14:15:51 +0000

    Out of date for GenericItem
    Helper method to determine if the item is past its sell by date
    Quality degrades twice as fast when item is out of date
    Use constants for magic numbers

--- lib/generic_item.rb
+++ lib/generic_item.rb
@@ -1,11 +1,15 @@
 require_relative 'item'
 
+IN_DATE_QUALITY_INCREMENT = -1
+OUT_OF_DATE_QUALITY_INCREMENT = -2
+SELL_IN_INCREMENT = -1
+
 # GenericItem
 class GenericItem < Item
   def initialize(name, sell_in, quality)
     super(name, sell_in, quality)
-    @daily_quality_increment = -1
-    @daily_sell_in_increment = -1
+    @daily_quality_increment = IN_DATE_QUALITY_INCREMENT
+    @daily_sell_in_increment = SELL_IN_INCREMENT
   end
 
   def update
@@ -14,6 +18,7 @@ class GenericItem < Item
   end
 
   def update_quality
+    @daily_quality_increment = OUT_OF_DATE_QUALITY_INCREMENT if sell_by_date_passed?
     @quality += @daily_quality_increment
     @quality = 0 if @quality < 0
     @quality = 50 if @quality > 50
@@ -22,4 +27,11 @@ class GenericItem < Item
   def update_sell_in
     @sell_in += @daily_sell_in_increment
   end
+
+  private
+
+  def sell_by_date_passed?
+    @sell_in <= 0
+  end
+
 end
